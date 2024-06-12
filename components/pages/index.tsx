eee1adcec15db5caf53f5fd6aa888749d86195e5
likuor <likuor0528@gmail.com>
2024-06-12 11:55:48 -0700

    add: showing answers functions

--- components/pages/index.tsx
+++ components/pages/index.tsx
@@ -2,21 +2,38 @@
 
 import React, { useState } from 'react';
 import { MathForm } from '../feature/form/MathForm';
+import { Button } from '../ui/button';
 
 const HomePage = () => {
   const [questions, setQuestions] = useState<any[]>([]);
-  console.log(questions);
+  const [isShownAnswers, setIsShownAnswers] = useState<boolean>(false);
 
   return (
     <div className='space-y-6'>
       <h1 className='font-bold text-xl'>ようこそMath Makerへ</h1>
       <MathForm setQuestions={setQuestions} />
       <div className='space-y-6'>
-        {questions.length > 0 && <h1 className='font-bold text-xl'>問題</h1>}
+        {questions.length > 0 && (
+          <div className='flex flex-row gap-8 items-center'>
+            <h1 className='font-bold text-xl'>問題</h1>
+            <Button
+              type='button'
+              variant='destructive'
+              onClick={() => setIsShownAnswers((prev) => !prev)}
+            >
+              答えを表示
+            </Button>
+          </div>
+        )}
         <div className='flex flex-col gap-6'>
-          {questions.map((question) => (
+          {questions.map((question, index) => (
             <div key={question.equation}>
-              <span>{question.equation}</span>
+              <span>
+                問題{index + 1}: {question.equation}
+              </span>
+              {isShownAnswers && (
+                <span className='text-destructive'> {question.answer}</span>
+              )}
             </div>
           ))}
         </div>
