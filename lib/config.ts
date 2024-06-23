8b67d503b70606ea19baa1afd6e5f4ecaf614168
likuor <likuor0528@gmail.com>
2024-06-23 12:54:13 -0700

    fix: meta google tag manager

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 const config = {
   appBaseUrl: process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000',
   google_analytics: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
-  google_GTMs: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER,
+  google_tag_manager: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER ?? '',
 };
 
 export default config;
